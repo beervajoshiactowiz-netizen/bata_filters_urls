1bb6c98c84d09cdd12337baa00ac1b185b39c2ea
beervajoshiactowiz-netizen <beervajoshi.actowiz@gmail.com>
2026-03-20 10:14:40 +0530

    added color filter , and combo of url with color

--- parser.py
+++ parser.py
@@ -10,21 +10,25 @@ def parser(text):
     sizes_path = tree.xpath(XPATHS["sizes_path"])
     discount_path = tree.xpath(XPATHS["discount_path"])
     brands_path = tree.xpath(XPATHS["brands_path"])
+    colors_paths=tree.xpath(XPATHS["colors_path"])
 
     sizes = sorted(set(sizes_path),key=int)
     discounts = sorted(set(discount_path))
     brands = sorted(set(b.strip() for b in brands_path if b.strip()))
+    colors=sorted(set(colors_paths))
 
     base_url="https://www.bata.com/in/men/shoes/slippers-e-flipflop"
     urls=[]
     for brand in brands:
-        for size in sizes:
-            for discount in discounts:
-                url=f"{base_url}/{brand.replace(" ","-").lower()}/{size}/?prefn1=discountPercent&prefv1={discount.replace("%","%25").replace(" ","%20")}"
-                urls.append({
-                    "brand":brand,
-                    "size":size,
-                    "discount":discount,
-                    "url":url
-                })
+        for color in colors:
+            for size in sizes:
+                for discount in discounts:
+                    url=f"{base_url}/{brand.replace(" ","-").lower()}/{color.replace(" ","-").lower()}/{size}/?prefn1=discountPercent&prefv1={discount.replace("%","%25").replace(" ","%20")}"
+                    urls.append({
+                        "brand":brand,
+                        "color":color,
+                        "size":size,
+                        "discount":discount,
+                        "url":url
+                    })
     return urls
